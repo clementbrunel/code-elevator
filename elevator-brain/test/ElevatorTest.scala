624e860f6dcd4f41d5fdad305eb901aae0d3946e
clement brunel <clement.brunel@intech.lu>
2013-10-23 00:58:07 +0200

    Implantation test

---
+++ elevator-brain/test/ElevatorTest.scala
@@ -0,0 +1,18 @@
+
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.mvc._
+import play.api._
+import org.scalatest._
+import org.scalatest.matchers._
+import scala.collection.mutable.Stack
+import play.api.Play.current
+
+
+class ElevatorTest extends FunSuite{
+	test("Test the Unit test module") {
+	    assert(1 === 1)
+	 }
+}
+
